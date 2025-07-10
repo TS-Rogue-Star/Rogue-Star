2ad80ac7a98fa7a3409a7521e2b26e2c8043ebf9
Kashargul <144968721+Kashargul@users.noreply.github.com>
2025-07-10 02:30:52 +0200

    use normal desc

--- tgui/packages/tgui/interfaces/Medigun/MedigunTabs/MedigunParts.tsx
+++ tgui/packages/tgui/interfaces/Medigun/MedigunTabs/MedigunParts.tsx
@@ -1,5 +1,4 @@
 import { Box, LabeledList, Stack } from '../../../components';
-import { MedigunDescription } from '../MedigunHelpers/MedigunDescription';
 import { ExamineData } from '../types';
 
 export const MedigunParts = (props: { examineData: ExamineData }, context) => {
@@ -12,82 +11,83 @@ export const MedigunParts = (props: { examineData: ExamineData }, context) => {
         <LabeledList>
           <LabeledList.Item label="Scanning Module">
             {smodule ? (
-              <MedigunDescription
-                part_1={'It has a'}
-                name={smodule.name}
-                part_2={'installed, device will function within'}
-                func={smodule.range + ' tiles'}
-                part_3={smodule.rating >= 5 ? 'and' : undefined}
-                extra={smodule.rating >= 5 ? 'through walls' : undefined}
-              />
+              <Box>
+                {'It has a ' +
+                  smodule.name +
+                  ' installed, device will function within ' +
+                  smodule.range +
+                  ' tiles'}
+                {smodule.rating >= 5 ? ' and' : undefined}
+                {smodule.rating >= 5 ? ' through walls' : undefined}
+                {'.'}
+              </Box>
             ) : (
               <Box color="red">Missing</Box>
             )}
           </LabeledList.Item>
           <LabeledList.Item label="Manipulator">
             {smanipulator ? (
-              <MedigunDescription
-                part_1={'It has a'}
-                name={smanipulator.name}
-                part_2={'installed, chem digitizing is now'}
-                func={
-                  smanipulator.rating >= 5
-                    ? '125% Efficient'
-                    : (smanipulator.rating / 4) * 100 + '% Efficient'
-                }
-              />
+              <Box>
+                {'It has a ' +
+                  smanipulator.name +
+                  ' installed, chem digitizing is now '}
+                {smanipulator.rating >= 5
+                  ? '125% Efficient'
+                  : (smanipulator.rating / 4) * 100 + '% Efficient'}
+                {'.'}
+              </Box>
             ) : (
               <Box color="red">Missing</Box>
             )}
           </LabeledList.Item>
           <LabeledList.Item label="Laser">
             {slaser ? (
-              <MedigunDescription
-                part_1={'It has a'}
-                name={slaser.name}
-                part_2={'installed, and can heal'}
-                func={slaser.rating + ' damage per cycle'}
-                part_3={slaser.rating >= 5 ? 'and will' : undefined}
-                extra={
-                  slaser.rating >= 5 ? 'stop bleeding and pain' : undefined
-                }
-                part_4={slaser.rating >= 5 ? 'while beam is focused' : ''}
-              />
+              <Box>
+                {'It has a ' +
+                  slaser.name +
+                  ' installed, and can heal ' +
+                  slaser.rating +
+                  ' damage per cycle '}
+                {slaser.rating >= 5 ? ' and will' : undefined}
+                {slaser.rating >= 5 ? ' stop bleeding and pain' : undefined}
+                {slaser.rating >= 5 ? ' while beam is focused' : ''}
+                {'.'}
+              </Box>
             ) : (
               <Box color="red">Missing</Box>
             )}
           </LabeledList.Item>
           <LabeledList.Item label="Capacitor">
             {scapacitor ? (
-              <MedigunDescription
-                part_1={'It has a'}
-                name={scapacitor.name}
-                part_2={'installed, battery charge will now drain at'}
-                func={scapacitor.chargecost + ' per second'}
-                part_3={
-                  scapacitor.rating >= 5
-                    ? 'the cell will recharge from the local power grid, it also grants a heal charge capacity of'
-                    : 'and grants a heal charge capacity of'
-                }
-                extra={scapacitor.tankmax + ' per type'}
-              />
+              <Box>
+                {'It has a ' +
+                  scapacitor.name +
+                  ' installed, battery charge will now drain at ' +
+                  scapacitor.chargecost +
+                  ' per second '}
+                {scapacitor.rating >= 5
+                  ? ' the cell will recharge from the local power grid, it also grants a heal charge capacity of '
+                  : ' and grants a heal charge capacity of '}
+                + {scapacitor.tankmax + ' per type'}
+                {'.'}
+              </Box>
             ) : (
               <Box color="red">Missing</Box>
             )}
           </LabeledList.Item>
           <LabeledList.Item label="Capacitor">
             {sbin ? (
-              <MedigunDescription
-                part_1={'It has a'}
-                name={sbin.name}
-                part_2={'installed, can hold '}
-                func={sbin.chemcap + ' reserve chems'}
-                part_3={
-                  sbin.rating >= 5
-                    ? 'and will slowly generate chems in exchange for power'
-                    : undefined
-                }
-              />
+              <Box>
+                {'It has a ' +
+                  sbin.name +
+                  ' installed, can hold ' +
+                  sbin.chemcap +
+                  ' reserve chems '}
+                {sbin.rating >= 5
+                  ? 'and will slowly generate chems in exchange for power'
+                  : undefined}
+                {'.'}
+              </Box>
             ) : (
               <Box color="red">Missing</Box>
             )}
