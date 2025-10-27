341cc70653e42bd82a33cad147eab6e921518360
Dameon Owen <dameonowen@yahoo.com>
2025-10-27 19:38:20 -0400

    Update _build_dependencies.sh

--- _build_dependencies.sh
+++ _build_dependencies.sh
@@ -2,7 +2,7 @@
 # For dreamchecker
 export SPACEMAN_DMM_VERSION=suite-1.10 #RS Edit: Updated from version 1.7 to 1.10 (Lira, August 2025)
 # For NanoUI + TGUI
-export NODE_VERSION=20
+export NODE_VERSION=20 #RS EDIT
 # Byond Major
 export BYOND_MAJOR=516 #RS EDIT
 # Byond Minor
@@ -10,4 +10,4 @@ export BYOND_MINOR=1661 #RS EDIT
 # Macro Count
 export MACRO_COUNT=4
 # Rust-G Version
-export RUST_G_VERSION=4.1.0
+export RUST_G_VERSION=4.1.0 #RS ADD
